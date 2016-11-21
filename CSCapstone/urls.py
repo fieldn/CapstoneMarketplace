cd744f5f35549600585dbcc73878c5d67cc88f20
Eric Buedel <ebuedel@purdue.edu>
2016-11-21 15:01:16 -0600

    milestone 1

--- CSCapstone/urls.py
+++ CSCapstone/urls.py
@@ -11,9 +11,10 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^', include('AuthenticationApp.urls')),
-	url(r'^', include('CSCapstoneApp.urls')),
-	url(r'^', include('ProjectsApp.urls')),
+	url(r'^', include('CommentsApp.urls')),
     url(r'^', include('CompaniesApp.urls')),
+	url(r'^', include('CSCapstoneApp.urls')),
     url(r'^', include('GroupsApp.urls')),
+	url(r'^', include('ProjectsApp.urls')),
 	url(r'^', include('UniversitiesApp.urls')),
 ]
