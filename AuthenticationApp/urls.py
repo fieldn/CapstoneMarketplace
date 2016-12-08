c9e09dd6c453ca3d92d5cfeffd50f494fec330cf
Noah Field <fieldn@purdue.edu>
2016-12-08 05:10:01 -0500

    Cleaned up login/authentication

--- AuthenticationApp/urls.py
+++ AuthenticationApp/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     url(r'^register$', views.auth_register, name='Register'),
     url(r'^update$', views.update_profile, name='UpdateProfile'),    
     url(r'^user$', views.view_profile, name='ViewProfile'),    
+    url(r'^register_student$', views.register_student, name='AuthStudent'),
+    url(r'^register_teacher$', views.register_teacher, name='AuthTeacher'),
+    url(r'^register_engineer$', views.register_engineer, name='AuthEngineer'),
+
 ]
